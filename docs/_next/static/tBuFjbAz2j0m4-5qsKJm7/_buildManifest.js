18dd9b54cd1cfe00d42d9d7e56bfd4c4d77eb642
kenkoooo <kenkou.n@gmail.com>
2023-03-26 15:11:40 +0900

    update

---
+++ docs/_next/static/tBuFjbAz2j0m4-5qsKJm7/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/294-e9f5ac1c3f0fc71c.js","static/chunks/pages/index-13aa7a83d049403f.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
